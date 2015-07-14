a1e5ebaa001b973a244e4b7bd2919d652c386616
fdiba <florent.dibartolo@gmail.com>
2015-07-14 11:38:22 +0200

    add menu

--- imeb/script.js
+++ imeb/script.js
@@ -1,10 +1,38 @@
 var names = [];
+var years = [];
 
 d3.csv("data/smallData.csv", function(data){
 
 	console.log(data);
 
-	for(key in data) names.push(data[key].name);
+	for(key in data) {
+
+		names.push(data[key].name);
+
+		if(years.length>0){
+
+			var hasBeenAdded = false;
+
+			var str = data[key].year;
+			
+
+			for(var j=0; j<years.length; j++){
+
+				var n = years[j].search(str);
+				if(n>=0) {
+					hasBeenAdded = true;
+					break;
+				}
+			}
+
+			if(!hasBeenAdded)years.push(data[key].year);
+
+
+		} else {
+			years.push(data[key].year);
+		}
+
+	}
 
 	var height = 400,
         width = 600,
@@ -36,9 +64,25 @@ d3.csv("data/smallData.csv", function(data){
     	.attr('x', '10')
     	.attr('y', '0')
     	.attr('dy', '15')
-    	.attr('fill', 'white')
-    	.attr('font-family', 'sans-serif')
-    	.attr('font-size', '10px')
+    	.style('fill', 'white')
+    	.style('font-family', 'sans-serif')
+    	.style('font-size', '10px')
     	.text(function(d,i) { return data[i].name; });
 
+    createMenu(years);
+
 });
+
+function createMenu(years){
+
+	for(var i=0; i<years.length; i++){
+
+		d3.select('.container').append('div')
+			//.text(function () { return objects.length; });
+			.style('display', 'inline-block')
+			//.style('background-color', 'teal')
+			.style('padding-right', '5px')
+			.text(function () { return years[i]; });
+
+	}
+}
