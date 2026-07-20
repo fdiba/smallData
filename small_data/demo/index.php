f5e2b16f00810bb3846d76be11890910dcbba33f
fdiba <florent.dibartolo@gmail.com>
2026-07-20 23:56:03 +0200

    index how to read added

--- small_data/demo/index.php
+++ small_data/demo/index.php
@@ -56,6 +56,35 @@
 		    <ul id="titles"></ul>
 		    <ul id="results"></ul>
 	    </div>
+		<div id="legend">
+			<p class="lg-title">How to read this page</p>
+			<div class="lg-cols">
+				<div>
+					<p><strong>The index</strong></p>
+					<ul>
+						<li>each composer is a grey square followed by one coloured square per participation in the Bourges competitions</li>
+						<li>the hue encodes the edition year, from red (1973) to pink (2009)</li>
+						<li>washed-out squares mark composers with no archived work; vivid ones have recordings in the IMEB capsules</li>
+					</ul>
+				</div>
+				<div>
+					<p><strong>Selection &amp; search</strong></p>
+					<ul>
+						<li>click a square to select a composer: the orange box sums up their participations and the purple box lists their archived works</li>
+						<li>type a name in <em>composer name</em> to list matching composers; click a result to highlight their squares in yellow</li>
+						<li><em>num of records &gt;=</em> rebuilds the index with only the composers having at least that many archived works</li>
+					</ul>
+				</div>
+				<div>
+					<p><strong>Navigation trace</strong></p>
+					<ul>
+						<li>every composer you consult joins the small canvas as a bubble, grouped by country &mdash; your navigation trace</li>
+						<li>the green box counts the composers consulted so far; click a bubble to detail one country</li>
+						<li>the trace is saved and can be replayed on the <em>Network</em> page with <em>compute traces</em></li>
+					</ul>
+				</div>
+			</div>
+		</div>
  	</div>
 </body>
 </html>
